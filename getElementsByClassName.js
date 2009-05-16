b9f022e48841aa1be79b67d517457aac306985e6
James Padolsey <jamespadolsey@gmail.com>
2009-05-16 09:16:08 +0100

    finished prelim getElementsByClassName

--- getElementsByClassName.js
+++ getElementsByClassName.js
@@ -6,6 +6,19 @@
 
 function getElementsByClassName(className, tagName, context) {
     
+    if (!className) {
+        throw new Error('getElementsByClassName: className not specified');
+    }
     
+    var all = (context || document).getElementsByTagName(tagName || '*'),
+        out = [];
+    
+    for (var i = 0, l = all.length; i < l; i++) {
+        if ( (' ' + all[i].className + ' ').indexOf(' ' + className + ' ') > -1 ) {
+            out[out.length] = all[i];
+        }
+    }
+    
+    return out;
     
 }
